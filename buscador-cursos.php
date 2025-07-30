16527ff4625792bcd5284630e88e8e693d8ee669
Alexandre Bruno Cosmo <abcosmo04@.com>
2025-07-30 20:25:32 -0300

    Introdução ao composer

--- buscador-cursos.php
+++ buscador-cursos.php
@@ -2,20 +2,16 @@
 
 require "vendor/autoload.php";
 
+use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client();
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler;
 
-$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
-
-$html = $resposta->getBody();
-
-$crawler = new Crawler();
-$crawler->addHtmlContent($html);
-
-$cursos = $crawler->filter('span.card-curso__nome');
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
-    echo $curso->textContent . PHP_EOL;
+    echo $curso . PHP_EOL;
 }
